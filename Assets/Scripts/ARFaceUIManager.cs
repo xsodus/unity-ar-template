f996e49d2326b96231692177948af256ffd9dacd
akkaponsomjai <asomjai@palo-it.com>
2024-05-05 22:11:00 +0700

    feat: :art: Install ARCore and add debug UI

---
+++ Assets/Scripts/ARFaceUIManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ARFaceUIManager : MonoBehaviour
+{
+     [SerializeField]
+     RectTransform leftSidebar;
+    [SerializeField]
+    Button rotateButton;
+
+    [SerializeField]
+    ARCameraManager arCameraManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Vector3 iconRotation = new Vector3(0,0,leftSidebar.rotation.eulerAngles.z);
+       iconRotation.z -= iconRotation.z;
+       rotateButton.GetComponent<RectTransform>().rotation = Quaternion.Euler(iconRotation);
+       rotateButton.onClick.AddListener(() => {
+          switch (arCameraManager.currentFacingDirection){
+               case CameraFacingDirection.User:
+                    arCameraManager.requestedFacingDirection = CameraFacingDirection.World;
+                    break;
+               default:
+                    arCameraManager.requestedFacingDirection = CameraFacingDirection.User;
+                    break;
+          }
+       });
+    }
+}
